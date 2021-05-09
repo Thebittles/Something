d9e7f746e578fc6882142a2990bd3d1a13a5174f
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-05-08 22:11:15 -0500

    add Jumbotron components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-
-
+import React from "react";
+import jumboData from "./fixtures/jumbo";
+import Jumbotron from "./components/jumbotron/";
 
 export default function App() {
   return (
-    <>
-    <h1>Hear is the footer</h1>
-    <p> testing</p>
-    </>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={ item.id } direction={ item.direction }>
+          <Jumbotron.Pane>  
+            <Jumbotron.Title>{ item.title }</Jumbotron.Title>
+              <Jumbotron.SubTitle>{ item.subTitle }</Jumbotron.SubTitle>
+            </Jumbotron.Pane> 
+            <Jumbotron.Pane>  
+            <Jumbotron.Image src={ item.image } alt={ item.alt } />
+            </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
-

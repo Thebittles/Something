ddc0b58667d74d1f5b7a2655f594c4d45f9b6aa6
irisn <iris.nathan@g.austincc.edu>
2021-05-08 23:00:17 -0500

    in_inital compound components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-
+import react from 'react';
+import React from 'react';
+import { JumbotronContainer } from './containers/jumbotron'
 
 
 export default function App() {
   return (
-    <>
-
-    <h1>Hear is the footer</h1>
-    <p> testing</p>
-    <p>Britt added something</p>
-    <h1>Hear is the footer</h1>
-
-    </>
+    <JumbotronContainer />
+      
   );
 }
+
 
---
+++ client/src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family: 'Helvtica Neue', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoohting: grayscale;
+    background-color: black;
+    color: #333333;
+    font-size: 16px;
+  }
+`;

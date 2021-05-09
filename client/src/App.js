8dec23d8dc99c28496f1c530abb7cc36edc62868
Thebittles <Brittsdevlog@gmail.com>
2021-05-08 22:10:34 -0500

    before eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-
-
+import React from 'react';
+import jumboData from './fixtures/jumbo.json'
+import Jumbotron from './components/jumbotron'
 
 export default function App() {
   return (
-    <>
 
-    <h1>Hear is the footer</h1>
-    <p> testing</p>
-    <p>Britt added something</p>
-    <h1>Hear is the footer</h1>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
 
-    </>
   );
 }
 

7a47df82dac42936c55c868da887ef4d6454e434
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-05-15 14:55:31 -0500

    update jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {JumbotronContainer} from "./containers/jumbotron"
-
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <JumbotronContainer />
-  );
+  return <JumbotronContainer />;
 }

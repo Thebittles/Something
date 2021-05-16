3a47826dc77df542ab49c66582eb30bf426c12a1
irisn <iris.nathan@g.austincc.edu>
2021-05-16 00:09:16 -0500

    in_add footer and faq accordion menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import react from 'react';
 import React from 'react';
+import { FaqsContainer } from './containers/faqs';
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron'
 
 
 export default function App() {
   return (
-    <JumbotronContainer />
-      
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer /> 
+    </> 
   );
 }
 

05eb6b0a6b4bb7d27269760d3735caa63e95aca0
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-06-05 19:42:31 -0500

    add header and lib

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer} from "../containers/footer"
 import { FaqsContainer } from "../containers/faqs"
@@ -6,10 +7,23 @@ import { HeaderContainer } from "../containers/header"
 
 
 export default function Home() {
-  return <>
-      <HeaderContainer />
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+  return(
+  <>
+         <HeaderContainer>
+          <Feature>
+            <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+            <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+            <OptForm>
+              <OptForm.Input placeholder="Email address" />
+              <OptForm.Button>Try it now</OptForm.Button>
+              <OptForm.Break />
+              <OptForm.Text>Ready to watch? Enter your email to create or restart your    membership.</OptForm.Text>
+            </OptForm>
+          </Feature>
+        </HeaderContainer>
+        <JumbotronContainer/>
+        <FaqsContainer/>
+        <FooterContainer/>
     </>
+  )
 }
